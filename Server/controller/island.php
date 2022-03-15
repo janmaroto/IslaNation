bb91b0c94be03943ae7b59f9d85f1bc01605b205
Matriksc123 <avismendez725@gmail.com>
2022-03-15 19:14:41 +0100

    Aqui ta el pull

--- Server/controller/island.php
+++ Server/controller/island.php
@@ -1,23 +1,24 @@
 <?php
-class User {
-    private $island_id;
-    private $island_name;
-    private $island_desc;
-    private $island_area;
-    private $island_location; // Longitud, latitude and country.
-    private $user_email;
-    private $island_pop;
-    private $island_pics;  // All tree product pics and its flag.
-    private $island_price;
-    private $island_add_date;
-    private $island_owner;
-    private $island_visits;
+    class Island {
 
-    function __construct($params) {
+        private $island_id;
+        private $island_name;
+        private $island_desc;
+        private $island_area;
+        private $island_cords;
+        private $island_country;
+        private $island_pop;
+        private $island_pics;
+        private $island_flag;
+        private $island_price;
+        private $island_add_date;
+        private $island_owner_id;
+        private $island_visits;
 
+        function __construct($params) {
+
+        }
+        
     }
-    
-    
-}
 
 ?>
--- Server/controller/user.php
+++ Server/controller/user.php
@@ -1,16 +1,16 @@
 <?php
-class User {
-    private $user_id;
-    private $user_name;
-    private $user_pwd_hash;
-    private $user_email;
-    private $user_avatar;
+    class User {
+        
+        private $user_id;
+        private $user_name;
+        private $user_pwd_hash;
+        private $user_email;
+        private $user_avatar;
 
-    function __construct($params) {
+        function __construct($params) {
 
+        }
+        
     }
-    
-    
-}
 
 ?>

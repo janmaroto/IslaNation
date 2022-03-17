6134581ae2a84c3a0da94ad5ffef0113b9b21d74
Matriksc123 <avismendez725@gmail.com>
2022-03-17 16:54:36 +0100

    x api key

--- Server/index.php
+++ Server/index.php
@@ -4,5 +4,8 @@
     $url = rtrim($url, '/');
     $params = explode('/',$url);
     $body = json_decode(file_get_contents('php://input'));
+    if ($_SERVER['x-api-key']){
+        $x_api_key = $_SERVER['x-api-key'];
+    };
     $app = new App($params, $body, $_SERVER['REQUEST_METHOD']);
 ?>

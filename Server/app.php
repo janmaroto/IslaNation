c152e79eca31006376784c2df996dcff391727c2
janmaroto <jan@trencapins.org>
2022-03-19 18:33:58 +0100

    Primera versió del login que funciona

--- Server/app.php
+++ Server/app.php
@@ -1,11 +1,11 @@
 <?php
 class App {
-    // public function __construct($params, $body, $method, $x_api_key) {
-    public function __construct($params, $body) {
-        // $controller_name = strtolower(array_shift($params));
-        $controller_name = "login";
-        // array_unshift($params, $method);
-        // array_unshift($params, $x_api_key);
+    public function __construct($params, $body, $method, $x_api_key) {
+    // public function __construct($params, $body) {
+        $controller_name = strtolower(array_shift($params));
+        // $controller_name = "login";
+        array_unshift($params, $method);
+        array_unshift($params, $x_api_key);
         $file = "./controller/" . $controller_name . ".php";
         if (file_exists($file)) {
             require_once $file;
--- Server/model/login_model.php
+++ Server/model/login_model.php
@@ -2,52 +2,56 @@
 class Login_model {
     private $db;
     private $userLogin;
+    private $correct;
 
     function __construct() {
         require_once("./settings/connection.php");
         $this->db=Connection::connect();
-        $this->userLogin=array();
+        $this->userLogin=new stdClass();
+        $this->correct = false;
 
     }
     public function checkUser($body){
         $user = "john";
-        $pass = "jhonpass";
+        $pass = "johnpass";
         $sql = $this->db->prepare('SELECT username, id FROM users WHERE (username = :user OR email = :user) AND pwd_hash = :pass');
         $sql->bindParam(':user', $user);
         $sql->bindParam(':pass', $pass);
         $sql->execute();
+
         while ($row=$sql->fetch()){
-            array_unshift($this->userLogin->username, $row['username']);
-            array_unshift($this->userLogin->id, $row['id']);
+            $this->userLogin->username = $row['username'];
+            $this->userLogin->id = $row['id'];
+            $this->correct = true;
         }
-        if (count($this->userLogin) == 1) {
-            $this->uuid = generateUuid();
+        if ($this->correct) {
+            $this->userLogin->uuid = generateUuid();
+            $this->userLogin->message = "succes";
             $sql_i = "INSERT INTO uuids VALUES(:uuid, :user);";
             $data = [
                 'uuid'=> $this->userLogin->uuid,
-                'user'=>$this->$userLogin->id
+                'user'=>$this->userLogin->id
             ];
             $rs_i = $this->db->prepare($sql_i)->execute($data);
+        } else {
+            $this->userLogin->message = "Username or password are incorrect!";
         }
         return $this->userLogin;
-    }
+    } 
+    
+}
+function generateUuid() {
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
 
-    static function generateUuid($data = null) {
-        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
-        $data = $data ?? random_bytes(16);
-        assert(strlen($data) == 16);
-    
-        // Set version to 0100
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-        // Set bits 6-7 to 10
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-    
-        // Output the 36 character UUID.
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-    }
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
-    
-    
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
 ?>
